f57090e4aee426fe931d4e541d3c3b6c955e83b3
Oskar Wołk <oskar.wolk@scout24.com>
2017-05-09 11:20:18 +0200

    nick progress

--- app/src/main/java/training/com/movieapp/rest/MoveDbAPI.java
+++ app/src/main/java/training/com/movieapp/rest/MoveDbAPI.java
@@ -1,4 +1,25 @@
 package training.com.movieapp.rest;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import training.com.movieapp.BuildConfig;
+
 public class MoveDbAPI {
+
+    private OkHttpClient client = new OkHttpClient();
+
+    public ResponseBody getNickCage() throws IOException {
+        Request request = new Request.Builder()
+                .url("https://api.themoviedb.org/3/person/2963?api_key=" + BuildConfig.API_KEY)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body();
+    }
+
 }
---
+++ app/src/test/java/training/com/movieapp/rest/MovieDbAPITest.java
@@ -0,0 +1,19 @@
+package training.com.movieapp.rest;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+
+public class MovieDbAPITest {
+
+    @Test
+    public void test() throws IOException {
+        MoveDbAPI api = new MoveDbAPI();
+
+        assertTrue(api.getNickCage().string().contains("Nicolas Cage"));
+    }
+}
