b0520446e9792f152ffd0f69ae969b48ff060f9e
verzano <mitchell.v.berg@gmail.com>
2017-05-09 11:08:04 +0200

    made full screen scrollable textview

--- app/src/main/java/training/com/movieapp/MainActivity.java
+++ app/src/main/java/training/com/movieapp/MainActivity.java
@@ -2,6 +2,7 @@ package training.com.movieapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
