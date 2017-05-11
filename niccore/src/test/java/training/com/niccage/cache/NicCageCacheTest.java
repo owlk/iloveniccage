3492a53339a7e9a2995d4acb1db7c32ad949a3c2
Oskar Wołk <oskar.wolk@scout24.com>
2017-05-11 10:10:48 +0200

    test for nic

---
+++ niccore/src/test/java/training/com/niccage/cache/NicCageCacheTest.java
@@ -0,0 +1,62 @@
+package training.com.niccage.cache;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import training.com.niccage.rest.NicCageApi;
+import training.com.niccage.rest.model.NicCageDetails;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NicCageCacheTest {
+
+    @Mock
+    private NicCageApi nicCageApi;
+
+    private NicCageCache nicCageCache;
+
+    @Before
+    public void setUp() {
+        nicCageCache = new NicCageCache(nicCageApi);
+
+        Call<NicCageDetails> call = mock(Call.class);
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                ((Callback<NicCageDetails>) invocation.getArguments()[0]).onResponse(null, Response.success(new NicCageDetails()));
+                return null;
+            }
+        }).when(call).enqueue(any(Callback.class));
+
+        when(nicCageApi.getNickCage()).thenReturn(call);
+    }
+
+    @Test
+    public void testSubscriberCalled() {
+        //given
+        NicCageCache.Subscriber<NicCageDetails> subscriber = mock(NicCageCache.Subscriber.class);
+        nicCageCache.subscribeToNicCageDetails(subscriber);
+
+        //when
+        nicCageCache.loadNicCageDetails();
+
+        //then
+        verify(subscriber).call(any(NicCageDetails.class));
+    }
+
+}
