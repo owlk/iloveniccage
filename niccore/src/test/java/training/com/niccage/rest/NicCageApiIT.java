62b5c2535cc7ca993e0f84240e2efab750a2dba8
Oskar Wołk <oskar.wolk@scout24.com>
2017-05-11 10:43:07 +0200

    web mock nic test

---
+++ niccore/src/test/java/training/com/niccage/rest/NicCageApiIT.java
@@ -0,0 +1,51 @@
+package training.com.niccage.rest;
+
+import org.junit.Test;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import training.com.niccage.rest.model.NicCageDetails;
+import training.com.niccage.rest.model.NicCageMovies;
+import training.com.niccage.rest.model.SimilarMovies;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class NicCageApiIT {
+    private NicCageApi nicCageApi;
+
+    @Test
+    public void getNicCageDetails() throws Exception {
+        givenApiBuilt();
+
+        NicCageDetails details = nicCageApi.getNicCageDetails().execute().body();
+
+        assertEquals("Nicolas Cage", details.getName());
+    }
+
+    @Test
+    public void getNicCageMovies() throws Exception {
+        givenApiBuilt();
+
+        NicCageMovies movies = nicCageApi.getNicCageMovies().execute().body();
+
+        assertNotNull(movies);
+    }
+
+    @Test
+    public void getSimilarMovies() throws Exception {
+        givenApiBuilt();
+
+        SimilarMovies similarMovies = nicCageApi.getSimilarMovies(1722, 1, NicCageApi.API_KEY).execute().body();
+
+        assertNotNull(similarMovies);
+    }
+
+    private void givenApiBuilt() {
+         nicCageApi = new Retrofit.Builder()
+                .baseUrl(NicCageApi.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(NicCageApi.class);
+    }
+}
--- niccore/src/test/java/training/com/niccage/rest/NicCageApiTest.java
+++ niccore/src/test/java/training/com/niccage/rest/NicCageApiTest.java
@@ -1,51 +1,68 @@
 package training.com.niccage.rest;
 
+import org.junit.After;
 import org.junit.Test;
 
+import java.io.IOException;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import training.com.niccage.rest.model.NicCageDetails;
-import training.com.niccage.rest.model.NicCageMovies;
-import training.com.niccage.rest.model.SimilarMovies;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+
 
 public class NicCageApiTest {
     private NicCageApi nicCageApi;
+    private MockWebServer server;
+
+    @After
+    public void shutDownServer() throws IOException {
+        server.shutdown();
+    }
 
     @Test
     public void getNicCageDetails() throws Exception {
-        givenApiBuilt();
+        givenMockServerApiBuilt();
 
-        NicCageDetails details = nicCageApi.getNicCageDetails().execute().body();
+        NicCageDetails nicCageDetails = nicCageApi.getNicCageDetails().execute().body();
 
-        assertEquals("Nicolas Cage", details.getName());
+        assertEquals("Nicolas Cage", nicCageDetails.getName());
     }
 
-    @Test
-    public void getNicCageMovies() throws Exception {
-        givenApiBuilt();
 
-        NicCageMovies movies = nicCageApi.getNicCageMovies().execute().body();
+    private void givenMockServerApiBuilt() throws IOException {
+        server = new MockWebServer();
+        server.enqueue(new MockResponse().setBody(
+                "{\n" +
+                        "  \"adult\": false,\n" +
+                        "  \"also_known_as\": [\n" +
+                        "    \"Nicolas Coppola\",\n" +
+                        "    \"\\u5c3c\\u53e4\\u62c9\\u65af\\u00b7\\u51ef\\u5947\",\n" +
+                        "    \"\\u041d\\u0438\\u043a\\u043e\\u043b\\u0430\\u0441 \\u041a\\u0435\\u0439\\u0434\\u0436\"\n" +
+                        "  ],\n" +
+                        "  \"biography\": \"Bio.\",\n" +
+                        "  \"birthday\": \"1964-01-07\",\n" +
+                        "  \"deathday\": \"\",\n" +
+                        "  \"gender\": 2,\n" +
+                        "  \"homepage\": \"\",\n" +
+                        "  \"id\": 2963,\n" +
+                        "  \"imdb_id\": \"nm0000115\",\n" +
+                        "  \"name\": \"Nicolas Cage\",\n" +
+                        "  \"place_of_birth\": \"Long Beach, California, USA\",\n" +
+                        "  \"popularity\": 15.352389,\n" +
+                        "  \"profile_path\": \"\\/fW37Gbk5PJZuXvyZwtcr0cMwPKY.jpg\"\n" +
+                        "}"
+        ));
+        server.start();
 
-        assertNotNull(movies);
-    }
-
-    @Test
-    public void getSimilarMovies() throws Exception {
-        givenApiBuilt();
-
-        SimilarMovies similarMovies = nicCageApi.getSimilarMovies(1722, 1, NicCageApi.API_KEY).execute().body();
-
-        assertNotNull(similarMovies);
-    }
-
-    private void givenApiBuilt() {
-         nicCageApi = new Retrofit.Builder()
-                .baseUrl(NicCageApi.BASE_URL)
+        nicCageApi = new Retrofit.Builder()
+                .baseUrl(server.url("/3/").toString())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(NicCageApi.class);
     }
+
 }
